e058696237cb704f8ede2259fd874fcd59b5092e
Roger Deetz <rdeetz@gmail.com>
2022-08-12 18:31:10 -0400

    Start of the next generation.

---
+++ QUnoMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace QUnoMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ QUnoMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace QUnoMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
