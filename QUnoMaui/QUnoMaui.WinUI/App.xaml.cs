685a3c0f5ab688a29b1af6ee07bc91a6064ba6c5
Roger Deetz <rdeetz@gmail.com>
2024-07-05 09:25:42 -0400

    Updated default namespace.

--- QUnoMaui/QUnoMaui.WinUI/App.xaml.cs
+++ QUnoMaui/QUnoMaui.WinUI/App.xaml.cs
@@ -1,19 +1,11 @@
-﻿using Microsoft.UI.Xaml;
+﻿// <copyright file="App.xaml.cs" company="Mooville">
+//   Copyright © 2024 Roger Deetz. All rights reserved.
+// </copyright>
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace QUnoMaui.WinUI
+namespace Mooville.QUno.Maui.WinUI
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     public partial class App : MauiWinUIApplication
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             this.InitializeComponent();
@@ -21,5 +13,4 @@ namespace QUnoMaui.WinUI
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
-
 }
--- QUnoMaui/QUnoMaui.WinUI/MauiProgram.cs
+++ QUnoMaui/QUnoMaui.WinUI/MauiProgram.cs
@@ -1,13 +1,15 @@
-﻿namespace QUnoMaui.WinUI
+﻿// <copyright file="MauiProgram.cs" company="Mooville">
+//   Copyright © 2024 Roger Deetz. All rights reserved.
+// </copyright>
+
+namespace Mooville.QUno.Maui.WinUI
 {
     public static class MauiProgram
     {
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-
-            builder
-                .UseSharedMauiApp();
+            builder.UseSharedMauiApp();
 
             return builder.Build();
         }
